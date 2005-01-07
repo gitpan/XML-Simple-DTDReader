ed1ec7c190baf2794b49b7b559b9d718efddfdf8
Alex Vandiver <cpan@chmrr.net>
2005-01-07 11:39:37 +0000

    Import of ALEXMV/XML-Simple-DTDReader-0.01 from CPAN.
    gitpan-cpan-distribution: XML-Simple-DTDReader
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ALEXMV/XML-Simple-DTDReader-0.01.tar.gz
    gitpan-cpan-author:       ALEXMV
    gitpan-cpan-maturity:     released

---
+++ t/array.pl
@@ -0,0 +1,16 @@
+$VAR1 = {
+          'stuff' => [
+                     {
+                       'name' => 'Moose',
+                       'other' => []
+                     },
+                     {
+                       'name' => 'Thingy',
+                       'other' => ['Value']
+                     },
+                     {
+                       'name' => 'Third',
+                       'other' => ['Value', 'Value2']
+                     }
+                   ]
+        };
